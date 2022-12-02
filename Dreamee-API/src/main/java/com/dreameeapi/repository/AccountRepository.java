307a6ed8f8dd6e3d1f293e4efb482a0d47b9c669
Nguyễn Đức Nam <98157595+nguyenducnam2@users.noreply.github.com>
2022-12-02 12:28:42 +0700

    s

--- Dreamee-API/src/main/java/com/dreameeapi/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dreameeapi.repository;
-
-import com.dreameeapi.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-}
--- Dreamee-API/src/main/java/com/dreameeapi/service/AccountService.java
+++
@@ -1,6 +0,0 @@
-package com.dreameeapi.service;
-
-import com.dreameeapi.entity.Account;
-
-public interface AccountService extends MyService<Account> {
-}
